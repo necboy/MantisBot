2697078a843eb889b14cb6b5b894563b630c83a4
jackyjiang <necboy@163.com>
2026-02-28 02:20:34 +0800

    fix(auth): remove unused getAuthHeaders import from App.tsx

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -19,7 +19,7 @@ import { CommandPalette } from './components/CommandPalette';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 import i18n from './i18n';
-import { getAuthHeaders, appendTokenToWsUrl, appendTokenToUrl, authFetch, clearAuthToken } from './utils/auth';
+import { appendTokenToWsUrl, appendTokenToUrl, authFetch, clearAuthToken } from './utils/auth';
 import { generateUUID } from './utils/uuid';
 
 interface FileAttachment {
